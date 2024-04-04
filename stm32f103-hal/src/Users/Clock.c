348d8e4d875b6ea8a9d71656e759d76dc6294d3b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-05 00:40:06 +0800

    stm32f103-hal

---
+++ stm32f103-hal/src/Users/Clock.c
@@ -0,0 +1,123 @@
+#include"Clock.h"
+#include<stm32f1xx_hal_cortex.h>
+
+uint8_t systick_ctrl_count_flag()
+{
+	uint32_t masked = SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk;
+	return masked >> SysTick_CTRL_COUNTFLAG_Pos;
+}
+
+uint8_t systick_ctrl_clock_source()
+{
+	uint32_t masked = SysTick->CTRL & SysTick_CTRL_CLKSOURCE_Msk;
+	return masked >> SysTick_CTRL_CLKSOURCE_Pos;
+}
+
+uint8_t systick_ctrl_clock_source_is_hclk()
+{
+	return systick_ctrl_clock_source();
+}
+
+uint8_t systick_ctrl_clock_source_is_hclk_div8()
+{
+	return !systick_ctrl_clock_source();
+}
+
+void set_systick_ctrl_clock_source(uint8_t div8)
+{
+	/* 其实 HAL 中已经有一个 HAL_SYSTICK_CLKSourceConfig 函数用来干这个事了。
+	* 只不过 HAL_SYSTICK_CLKSourceConfig 函数不太清晰。
+	*/
+	if (div8)
+	{
+		// 清 0 表示使用 8 分频
+		SysTick->CTRL &= ~SYSTICK_CLKSOURCE_HCLK;
+	}
+	else
+	{
+		// 置 1 表示不分频，让 HCLK 直接作为 Systick 的时钟源。
+		SysTick->CTRL |= SYSTICK_CLKSOURCE_HCLK;
+	}
+}
+
+uint32_t systick_clock_source_freq()
+{
+	uint32_t hclk_freq = HAL_RCC_GetHCLKFreq();
+	if (systick_ctrl_clock_source_is_hclk_div8())
+	{
+		return hclk_freq / 8;
+	}
+
+	return hclk_freq;
+}
+
+uint32_t systick_load_reload()
+{
+	uint32_t masked = SysTick->LOAD & SysTick_LOAD_RELOAD_Msk;
+	return masked >> SysTick_LOAD_RELOAD_Pos;
+}
+
+uint32_t systick_val_current()
+{
+	uint32_t masked = SysTick->VAL & SysTick_VAL_CURRENT_Msk;
+	return masked >> SysTick_VAL_CURRENT_Pos;
+}
+
+void systick_nop_loop_delay_tick(uint32_t tick_count)
+{
+	/* 这里不禁用操作系统的调度。不要让此函数耦合性太强。
+	* 如果需要的话，禁用操作系统的调度这个操作应该放到本函数外，调用者自己执行。
+	*/
+	uint32_t old_tick = systick_val_current();
+	uint32_t total_tick = 0;
+	while (1)
+	{
+		uint32_t now_tick = systick_val_current();
+		if (old_tick == now_tick)
+		{
+			// CPU 太快了，导致再次进入循环后 old_tick == now_tick
+			continue;
+		}
+
+		uint32_t delta_tick = 0;
+		if (old_tick > now_tick)
+		{
+			// 因为是减计数，所以此时没有发生环绕
+			delta_tick = old_tick - now_tick;
+		}
+		else
+		{
+			// 发生了环绕
+			// delta_tick = old_tick - (now_tick - reload)
+			// delta_tick = old_tick - now_tick + reload
+			delta_tick = old_tick - now_tick + systick_load_reload();
+		}
+
+		total_tick += delta_tick;
+		if (total_tick >= tick_count)
+		{
+			return;
+		}
+
+		old_tick = now_tick;
+	}
+}
+
+void systick_nop_loop_delay_us(uint32_t us_count)
+{
+	uint32_t freq = systick_clock_source_freq();
+	systick_nop_loop_delay_tick(freq / (uint32_t)1e6 * us_count);
+}
+
+void systick_nop_loop_delay_ms(uint32_t ms_count)
+{
+	for (uint32_t i = 0; i < ms_count; i++)
+	{
+		systick_nop_loop_delay_us(1000);
+	}
+}
+
+void HAL_Delay(uint32_t Delay)
+{
+	systick_nop_loop_delay_ms(Delay);
+}
---
+++ stm32f103-hal/src/Users/Clock.h
@@ -0,0 +1,97 @@
+#pragma once
+#include"stm32f1xx_hal.h"
+#include<core_cm3.h>
+#include<stdint.h>
+#include<stm32f103xe.h>
+#include<stm32f1xx_hal_rcc.h>
+
+/// <summary>
+///		获取 SysTick 寄存器组中的 CTRL 寄存器的 COUNTFLAG 位的值。
+///		该位是只读的。
+/// </summary>
+/// 
+/// <note>
+///		SysTick 是一个减计数的计数器。如果自上次读取 SysTick 的值后，SysTick 减计数到 0 了，
+///		说明计数值要发生回绕了，此时 CTRL 寄存器的 COUNTFLAG 位会置位，则本函数返回 1.
+/// </note>
+/// <note>
+///		读取 COUNTFLAG 位会导致 COUNTFLAG 位自动清零。
+/// </note>
+/// 
+/// <returns>发生了回绕 返回非 0 值，没有发生回绕返回 0.</returns>
+uint8_t systick_ctrl_count_flag();
+
+#pragma region Systick时钟源相关
+/// <summary>
+///		CLKSOURCE 位为 0 表示使用 HCLK / 8 作为时钟源。
+///		CLKSOURCE 位为 1 表示使用 HCLK 直接作为时钟源。
+///		CLKSOURCE 位是可读可写的。
+/// </summary>
+/// <returns></returns>
+uint8_t systick_ctrl_clock_source();
+uint8_t systick_ctrl_clock_source_is_hclk();
+uint8_t systick_ctrl_clock_source_is_hclk_div8();
+
+/// <summary>
+///		选择 SysTick 的时钟源。
+/// </summary>
+/// <param name="div8">
+///		为 true 表示将 HCLK 8 分频后输入 SysTick。
+///		为 false 表示直接将 HCLK 输入 SysTick，不经过分频。
+/// </param>
+void set_systick_ctrl_clock_source(uint8_t div8);
+
+/// <summary>
+///		获取 Systick 的时钟源的频率
+/// </summary>
+/// <returns></returns>
+uint32_t systick_clock_source_freq();
+#pragma endregion
+
+/// <summary>
+///		获取 SysTick 的 LOAD 寄存器的 RELOAD 部分的值。
+///		RELOAD 占据 LOAD 寄存器的低 23 位。
+///		
+///		RELOAD 是用来在计数值递减到 0 后，下一个时钟周期装载到计数器中的。
+/// </summary>
+/// <returns></returns>
+uint32_t systick_load_reload();
+
+/// <summary>
+///		获取 SysTick 的 VAL 寄存器的 CURRENT 部分的值。
+///		CURRENT 部分占据了寄存器的低 23 位，表示当前计数值。
+/// </summary>
+/// <note>
+///		CURRENT 可读可写。只不过这里的写并不是如你所愿地将数据赋予 CURRENT。
+///		写入 CURRENT 会导致 CURRENT 清零，同时会导致 CTRL 寄存器的
+///		COUNTFLAG 位清零。
+/// </note>
+/// <returns>当前计数值</returns>
+uint32_t systick_val_current();
+
+#pragma region 利用SysTick进行延时
+/// <summary>
+///		通过空指令循环来延时
+/// </summary>
+/// <param name="tick_count">要延时的 SysTick 计数值</param>
+void systick_nop_loop_delay_tick(uint32_t tick_count);
+
+/// <summary>
+///		通过空指令循环来延时
+/// </summary>
+/// <param name="us_count">要延时多少微秒</param>
+void systick_nop_loop_delay_us(uint32_t us_count);
+
+/// <summary>
+///		通过无操作的循环来延时指定的毫秒数。
+///		此函数效率较低，如果有操作系统，尽量不要用此函数。
+/// </summary>
+/// <param name="ms_count"></param>
+void systick_nop_loop_delay_ms(uint32_t ms_count);
+
+/// <summary>
+///		重写 __weak 的 HAL_Delay 函数
+/// </summary>
+/// <param name="Delay"></param>
+void HAL_Delay(uint32_t Delay);
+#pragma endregion
