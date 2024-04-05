f242d994ad7cd44f0f0eacd05e2e8803fa658557
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-05 16:53:32 +0800

    将test添加到编译

--- stm32f103-hal/src/Drivers/SYSTEM/sys/sys.h
+++ stm32f103-hal/src/Drivers/SYSTEM/sys/sys.h
@@ -26,17 +26,18 @@
 #define __SYS_H
 
 #include "stm32f1xx.h"
+#include<stdint.h>
 
 
-/**
- * SYS_SUPPORT_OS用于定义系统文件夹是否支持OS
- * 0,不支持OS
- * 1,支持OS
- */
+ /**
+  * SYS_SUPPORT_OS用于定义系统文件夹是否支持OS
+  * 0,不支持OS
+  * 1,支持OS
+  */
 #define SYS_SUPPORT_OS          0
 
 
-/*函数申明*******************************************************************************************/
+  /*函数申明*******************************************************************************************/
 
 void sys_nvic_set_vector_table(uint32_t baseaddr, uint32_t offset);             /* 设置中断偏移量 */
 void sys_standby(void);                                                         /* 进入待机模式 */
