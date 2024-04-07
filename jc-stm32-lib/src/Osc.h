afd0f8cf64d754333b5262979bc78bb0360819b2
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-07 16:21:38 +0800

    PllMul

--- jc-stm32-lib/src/Osc.h
+++ jc-stm32-lib/src/Osc.h
@@ -5,3 +5,68 @@
 #include<stm32f1xx_hal_def.h>
 #include<stm32f1xx_hal_rcc.h>
 
+/// <summary>
+///		PLL 倍频系数
+/// </summary>
+enum class PllMul
+{
+	#ifdef RCC_PLL_MUL2
+	Mul2 = RCC_PLL_MUL2,
+	#endif // RCC_PLL_MUL2
+
+	#ifdef RCC_PLL_MUL3
+	Mul3 = RCC_PLL_MUL3,
+	#endif // RCC_PLL_MUL3
+
+	#ifdef RCC_PLL_MUL4
+	Mul4 = RCC_PLL_MUL4,
+	#endif // RCC_PLL_MUL4
+
+	#ifdef RCC_PLL_MUL5
+	Mul5 = RCC_PLL_MUL5,
+	#endif // RCC_PLL_MUL5
+
+	#ifdef RCC_PLL_MUL6
+	Mul6 = RCC_PLL_MUL6,
+	#endif // RCC_PLL_MUL6
+
+	#ifdef RCC_PLL_MUL7
+	Mul7 = RCC_PLL_MUL7,
+	#endif // RCC_PLL_MUL7
+
+	#ifdef RCC_PLL_MUL8
+	Mul8 = RCC_PLL_MUL8,
+	#endif // RCC_PLL_MUL8
+
+	#ifdef RCC_PLL_MUL9
+	Mul9 = RCC_PLL_MUL9,
+	#endif // RCC_PLL_MUL9
+
+	#ifdef RCC_PLL_MUL10
+	Mul10 = RCC_PLL_MUL10,
+	#endif // RCC_PLL_MUL10
+
+	#ifdef RCC_PLL_MUL11
+	Mul11 = RCC_PLL_MUL11,
+	#endif // RCC_PLL_MUL11
+
+	#ifdef RCC_PLL_MUL12
+	Mul12 = RCC_PLL_MUL12,
+	#endif // RCC_PLL_MUL12
+
+	#ifdef RCC_PLL_MUL13
+	Mul13 = RCC_PLL_MUL13,
+	#endif // RCC_PLL_MUL13
+
+	#ifdef RCC_PLL_MUL14
+	Mul14 = RCC_PLL_MUL14,
+	#endif // RCC_PLL_MUL14
+
+	#ifdef RCC_PLL_MUL15
+	Mul15 = RCC_PLL_MUL15,
+	#endif // RCC_PLL_MUL15
+
+	#ifdef RCC_PLL_MUL16
+	Mul16 = RCC_PLL_MUL16,
+	#endif // RCC_PLL_MUL16
+};
