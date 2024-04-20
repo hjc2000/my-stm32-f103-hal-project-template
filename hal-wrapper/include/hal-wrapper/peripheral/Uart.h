fe87765dc7f8911347ecc5199bcf63070a31e64b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-21 00:51:20 +0800

    region

--- hal-wrapper/include/hal-wrapper/peripheral/Uart.h
+++ hal-wrapper/include/hal-wrapper/peripheral/Uart.h
@@ -166,12 +166,7 @@ namespace hal
 		}
 		#pragma endregion
 
-		/// <summary>
-		///		接收完成时被回调
-		/// </summary>
-		void(*_on_receive_completed)() = nullptr;
-		UartReceiveCompletedHandler *_receive_completed_handler = nullptr;
-
+		#pragma region 接收
 		/// <summary>
 		///		每次在中断中接收数据后，接收中断都会被禁用，此时需要调用本函数重新启用。
 		/// </summary>
@@ -179,5 +174,12 @@ namespace hal
 		{
 			HAL_UART_Receive_IT(&_uart_handle, ReceiveBuffer(), ReceiveBufferSize());
 		}
+		#pragma endregion
+
+		/// <summary>
+		///		接收完成时被回调
+		/// </summary>
+		void(*_on_receive_completed)() = nullptr;
+		UartReceiveCompletedHandler *_receive_completed_handler = nullptr;
 	};
 }
