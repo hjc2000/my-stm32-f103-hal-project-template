700543699424e0d63d08cfe69130f16a20b49870
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-20 18:37:17 +0800

    Uart1

--- atk-stm32f103/include/atk-stm32f103/uart/Uart1.cpp
+++ atk-stm32f103/include/atk-stm32f103/uart/Uart1.cpp
@@ -1,23 +1,33 @@
-#include"Uart.h"
+#include"Uart1.h"
 
 using namespace atk;
+using namespace hal;
+
+#ifdef USART1
+void atk::Uart1::MspInit(UART_HandleTypeDef *huart)
+{
+	GpioPortA::Instance().EnableClock();
+
+}
 
 bool atk::Uart1::IsClockEnabled()
 {
-	return false;
+	return __HAL_RCC_USART1_IS_CLK_ENABLED();
 }
 
 void atk::Uart1::EnableClock()
 {
+	__HAL_RCC_USART1_CLK_ENABLE();
 }
 
 void atk::Uart1::DisableClock()
 {
+	__HAL_RCC_USART1_CLK_DISABLE();
 }
 
 hal::UartCallbackFunc atk::Uart1::MspInitCallback()
 {
-	return nullptr;
+	return MspInit;
 }
 
 hal::UartCallbackFunc atk::Uart1::ReceiveCompleteCallback()
@@ -39,3 +49,4 @@ uint16_t atk::Uart1::ReceiveBufferSize()
 {
 	return _receive_buffer_size;
 }
+#endif // USART1
--- atk-stm32f103/include/atk-stm32f103/uart/Uart1.h
+++ atk-stm32f103/include/atk-stm32f103/uart/Uart1.h
@@ -1,4 +1,5 @@
 #pragma once
+#include<hal-wrapper/peripheral/GpioPort.h>
 #include<hal-wrapper/peripheral/Uart.h>
 
 namespace atk
@@ -6,25 +7,31 @@ namespace atk
 	#ifdef USART1
 	class Uart1 :public hal::Uart
 	{
+		Uart1() = default;
+		Uart1 &operator=(Uart1 const &value) = delete;
+
 		uint8_t _receive_buffer[1]{};
 		uint16_t _receive_buffer_size = 1;
 
+		static void MspInit(UART_HandleTypeDef *huart);
+
 	public:
-		// 通过 Uart 继承
+		static Uart1 &Instance()
+		{
+			static Uart1 instance;
+			return instance;
+		}
+
 		bool IsClockEnabled() override;
-
 		void EnableClock() override;
-
 		void DisableClock() override;
 
 		hal::UartCallbackFunc MspInitCallback() override;
-
 		hal::UartCallbackFunc ReceiveCompleteCallback() override;
 
 		USART_TypeDef *HardwareInstance() override;
 
 		uint8_t *ReceiveBuffer() override;
-
 		uint16_t ReceiveBufferSize() override;
 
 	};
