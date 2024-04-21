b15bd5dee11ff84a0822a092d1c203aaa49a1f57
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-21 21:58:11 +0800

    Uart1DmaChannel

--- atk-stm32f103/include/atk-stm32f103/peripheral/dma/Uart1DmaChannel.cpp
+++ atk-stm32f103/include/atk-stm32f103/peripheral/dma/Uart1DmaChannel.cpp
@@ -1,5 +1,7 @@
 #include"Uart1DmaChannel.h"
 
+using namespace hal;
+
 bool atk::Uart1DmaChannel::IsClockEnabled()
 {
 	return __HAL_RCC_DMA1_IS_CLK_ENABLED();
@@ -18,6 +20,24 @@ void atk::Uart1DmaChannel::EnableClock()
 void atk::Uart1DmaChannel::DisableClock()
 {
 	__HAL_RCC_DMA1_CLK_DISABLE();
+}
+
+atk::Uart1DmaChannel::Uart1DmaChannel()
+{
+	EnableClock();
+
+	hal::DmaInitOptions options;
+	options._direction = DmaDataTransferDirection::MemoryToPeripheral;
+	options._peripheral_inc_mode = DmaPeripheralIncMode::Disable;
+	options._mem_inc_mode = DmaMemoryIncMode::Enable;
+	options._peripheral_data_alignment = PeripheralDataAlignment::Byte;
+	options._mem_data_alignment = MemoryDataAlignment::Byte;
+	options._mode = DmaMode::Normal;
+	options._priority = DmaPriority::Medium;
+
+	_handle.Instance = HardwareInstance();
+	_handle.Init = options;
+	HAL_DMA_Init(&_handle);
 }
 
 DMA_Channel_TypeDef *atk::Uart1DmaChannel::HardwareInstance()
--- atk-stm32f103/include/atk-stm32f103/peripheral/dma/Uart1DmaChannel.h
+++ atk-stm32f103/include/atk-stm32f103/peripheral/dma/Uart1DmaChannel.h
@@ -13,24 +13,7 @@ namespace atk
 		void DisableClock() override;
 
 	public:
-		Uart1DmaChannel()
-		{
-			using namespace hal;
-			EnableClock();
-
-			hal::DmaInitOptions options;
-			options._direction = DmaDataTransferDirection::MemoryToPeripheral;
-			options._peripheral_inc_mode = DmaPeripheralIncMode::Disable;
-			options._mem_inc_mode = DmaMemoryIncMode::Enable;
-			options._peripheral_data_alignment = PeripheralDataAlignment::Byte;
-			options._mem_data_alignment = MemoryDataAlignment::Byte;
-			options._mode = DmaMode::Normal;
-			options._priority = DmaPriority::Medium;
-
-			_handle.Instance = HardwareInstance();
-			_handle.Init = options;
-			HAL_DMA_Init(&_handle);
-		}
+		Uart1DmaChannel();
 
 		DMA_Channel_TypeDef *HardwareInstance() override;
 
