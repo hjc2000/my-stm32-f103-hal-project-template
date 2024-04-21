613433afa8817b44f0d887d4901ed0395acb115b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-21 19:43:54 +0800

    1

--- hal-wrapper/include/hal-wrapper/peripheral/uart/Uart.cpp
+++ hal-wrapper/include/hal-wrapper/peripheral/uart/Uart.cpp
@@ -3,38 +3,6 @@
 using namespace std;
 using namespace hal;
 
-#pragma region UartInitOptions
-hal::UartInitOptions::UartInitOptions(UART_InitTypeDef const &value)
-{
-	*this = value;
-}
-
-UartInitOptions &hal::UartInitOptions::operator=(UART_InitTypeDef const &value)
-{
-	_baud_rate = value.BaudRate;
-	_word_length = (UartWordLength)value.WordLength;
-	_stop_bit_count = (UartStopBitCount)value.StopBits;
-	_parity = (UartParity)value.Parity;
-	_mode = (UartMode)value.Mode;
-	_hw_flow_ctl = (UartHardwareFlowControl)value.HwFlowCtl;
-	_over_sampling = (UartOverSample)value.OverSampling;
-	return *this;
-}
-
-hal::UartInitOptions::operator UART_InitTypeDef() const
-{
-	UART_InitTypeDef def;
-	def.BaudRate = _baud_rate;
-	def.WordLength = (uint32_t)_word_length;
-	def.StopBits = (uint32_t)_stop_bit_count;
-	def.Parity = (uint32_t)_parity;
-	def.Mode = (uint32_t)_mode;
-	def.HwFlowCtl = (uint32_t)_hw_flow_ctl;
-	def.OverSampling = (uint32_t)_over_sampling;
-	return def;
-}
-#pragma endregion
-
 void hal::Uart::Initialize(UartInitOptions const &options)
 {
 	_uart_handle.Instance = HardwareInstance();
---
+++ hal-wrapper/include/hal-wrapper/peripheral/uart/UartInitOptions.cpp
@@ -0,0 +1,33 @@
+#include"UartInitOptions.h"
+
+using namespace hal;
+
+hal::UartInitOptions::UartInitOptions(UART_InitTypeDef const &value)
+{
+	*this = value;
+}
+
+UartInitOptions &hal::UartInitOptions::operator=(UART_InitTypeDef const &value)
+{
+	_baud_rate = value.BaudRate;
+	_word_length = (UartWordLength)value.WordLength;
+	_stop_bit_count = (UartStopBitCount)value.StopBits;
+	_parity = (UartParity)value.Parity;
+	_mode = (UartMode)value.Mode;
+	_hw_flow_ctl = (UartHardwareFlowControl)value.HwFlowCtl;
+	_over_sampling = (UartOverSample)value.OverSampling;
+	return *this;
+}
+
+hal::UartInitOptions::operator UART_InitTypeDef() const
+{
+	UART_InitTypeDef def;
+	def.BaudRate = _baud_rate;
+	def.WordLength = (uint32_t)_word_length;
+	def.StopBits = (uint32_t)_stop_bit_count;
+	def.Parity = (uint32_t)_parity;
+	def.Mode = (uint32_t)_mode;
+	def.HwFlowCtl = (uint32_t)_hw_flow_ctl;
+	def.OverSampling = (uint32_t)_over_sampling;
+	return def;
+}
