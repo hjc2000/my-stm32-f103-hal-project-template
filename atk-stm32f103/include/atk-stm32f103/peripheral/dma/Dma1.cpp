48a2d6330dfa5e5a39bc824408a6d2e8ad2688ef
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-21 12:53:25 +0800

    DmaInitOptions

--- atk-stm32f103/include/atk-stm32f103/peripheral/dma/Dma1.cpp
+++ atk-stm32f103/include/atk-stm32f103/peripheral/dma/Dma1.cpp
@@ -1,2 +1,16 @@
 #include"Dma1.h"
 
+bool atk::Dma1::IsClockEnabled()
+{
+	return __HAL_RCC_DMA1_IS_CLK_ENABLED();
+}
+
+void atk::Dma1::EnableClock()
+{
+	__HAL_RCC_DMA1_CLK_ENABLE();
+}
+
+void atk::Dma1::DisableClock()
+{
+	__HAL_RCC_DMA1_CLK_DISABLE();
+}
--- atk-stm32f103/include/atk-stm32f103/peripheral/dma/Dma1.h
+++ atk-stm32f103/include/atk-stm32f103/peripheral/dma/Dma1.h
@@ -3,9 +3,12 @@
 
 namespace atk
 {
-	class Dma1
+	class Dma1 :public hal::Dma
 	{
 	public:
+		bool IsClockEnabled() override;
+		void EnableClock() override;
+		void DisableClock() override;
 
 	};
 }
