e41526df707f594c1576faef86f9eccc166237cd
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-21 21:03:40 +0800

    IPeripheral

--- hal-wrapper/include/hal-wrapper/peripheral/IPeripheral.h
+++ hal-wrapper/include/hal-wrapper/peripheral/IPeripheral.h
@@ -3,6 +3,14 @@
 
 namespace hal
 {
+	/// <summary>
+	///		外设接口
+	/// </summary>
+	/// <typeparam name="HardwareTypeDef">
+	///		HAL 库中为外设定义的结构体。利用结构体字段地址偏移量自动递增的特性，
+	///		使得结构体字段与寄存器一一对应，通过将外设基地址强制转换为结构体指针，
+	///		即可通过访问结构体对象字段的方式读写寄存器。
+	/// </typeparam>
 	template<typename HardwareTypeDef>
 	class IPeripheral
 	{
