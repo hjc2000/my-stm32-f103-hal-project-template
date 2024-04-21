3faed58e542d92e8c94966685a4ff610000546da
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-21 19:29:45 +0800

    IHandleWrapper

--- hal-wrapper/include/hal-wrapper/IHandleWrapper.h
+++ hal-wrapper/include/hal-wrapper/IHandleWrapper.h
@@ -2,16 +2,6 @@
 
 namespace hal
 {
-	/// <summary>
-	///		如果一个类内部含有名称类似于 DMA_HandleTypeDef 这种 XXX_HandleTypeDef
-	///		的 HAL 库句柄对象，则继承本接口。
-	/// 
-	///		但是，必须是自己包装的句柄才能够继承本接口并将句柄的指针返回。例如，只有 DMA
-	///		的包装类才能继承 IHandleWrapper<DMA_HandleTypeDef> 类，其他类，例如串口类，
-	///		虽然也有 DMA_HandleTypeDef* 的字段，但是它并不能继承
-	///		IHandleWrapper<DMA_HandleTypeDef> 类。
-	/// </summary>
-	/// <typeparam name="HandleType"></typeparam>
 	template<typename HandleType>
 	class IHandleWrapper
 	{
