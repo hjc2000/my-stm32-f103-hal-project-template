d004169d1045b613cc0a15366eecbf30b56c837f
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-22 15:52:34 +0800

    导入DSP库

---
+++ stm32f103-hal/src/Drivers/CMSIS/DSP/Projects/GCC/IntermediateFiles/cortexM0l/arm_add_q31.d
@@ -0,0 +1,29 @@
+intermediatefiles/cortexm0l/arm_add_q31.o: \
+ ../../Source/BasicMathFunctions/arm_add_q31.c ../../Include/arm_math.h \
+ ../../../Core/Include/core_cm0.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/stdint.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ ../../../Core/Include/cmsis_version.h \
+ ../../../Core/Include/cmsis_compiler.h ../../../Core/Include/cmsis_gcc.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/string.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/_ansi.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/newlib.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/config.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/machine/ieeefp.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/reent.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/_ansi.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/cdefs.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/_types.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/machine/_types.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/lock.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/_locale.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/strings.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/sys/string.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/c++/13.2.1/math.h \
+ D:/bin/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/arm-none-eabi/include/math.h
---
+++ stm32f103-hal/src/Drivers/CMSIS/DSP/Projects/GCC/IntermediateFiles/cortexM0l/arm_bitreversal2.d
@@ -0,0 +1,2 @@
+intermediatefiles/cortexm0l/arm_bitreversal2.o: \
+ ../../Source/TransformFunctions/arm_bitreversal2.S
