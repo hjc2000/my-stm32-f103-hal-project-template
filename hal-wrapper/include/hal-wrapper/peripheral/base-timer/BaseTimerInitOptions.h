14c9d0d3cd675da53d58581b89b0d350e4ca3454
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-24 15:52:12 +0800

    BaseTimerInitOptions

---
+++ hal-wrapper/include/hal-wrapper/peripheral/base-timer/BaseTimerInitOptions.h
@@ -0,0 +1,59 @@
+#pragma once
+#include<hal.h>
+
+namespace hal
+{
+	class BaseTimerInitOptions
+	{
+	public:
+		/// <summary>
+		///		预分频器。范围：[0x0000, 0xFFFF]
+		/// </summary>
+		uint32_t _prescaler;
+
+		enum class CounterMode
+		{
+			Up = TIM_COUNTERMODE_UP,
+			Down = TIM_COUNTERMODE_DOWN,
+			CenterAligned1 = TIM_COUNTERMODE_CENTERALIGNED1,
+			CenterAligned2 = TIM_COUNTERMODE_CENTERALIGNED2,
+			CenterAligned3 = TIM_COUNTERMODE_CENTERALIGNED3,
+		};
+
+		CounterMode _counter_mode;       /*!< Specifies the counter mode.
+		This parameter can be a value of @ref TIM_Counter_Mode */
+
+		/// <summary>
+		///		自动重载值。范围：[0x0000, 0xFFFF]
+		/// </summary>
+		uint32_t _period;
+
+		enum class ClockDivision
+		{
+			Div1 = TIM_CLOCKDIVISION_DIV1,
+			Div2 = TIM_CLOCKDIVISION_DIV2,
+			Div4 = TIM_CLOCKDIVISION_DIV4,
+		};
+
+		ClockDivision _clock_division;
+
+		uint32_t RepetitionCounter;  /*!< Specifies the repetition counter value. Each time the RCR downcounter
+		reaches zero, an update event is generated and counting restarts
+		from the RCR value (N).
+		This means in PWM mode that (N+1) corresponds to:
+		- the number of PWM periods in edge-aligned mode
+		- the number of half PWM period in center-aligned mode
+		GP timers: this parameter must be a number between Min_Data = 0x00 and
+		Max_Data = 0xFF.
+		Advanced timers: this parameter must be a number between Min_Data = 0x0000 and
+		Max_Data = 0xFFFF. */
+
+		enum class AutoReloadPreload
+		{
+			Enable = TIM_AUTORELOAD_PRELOAD_ENABLE,
+			Disable = TIM_AUTORELOAD_PRELOAD_DISABLE,
+		};
+
+		AutoReloadPreload _auto_reload_preload;
+	};
+}
