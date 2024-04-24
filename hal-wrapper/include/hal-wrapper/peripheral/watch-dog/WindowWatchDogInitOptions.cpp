79fa425a8df08275e92caddcf408006b824d760e
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-24 11:03:39 +0800

    WindowWatchDogInitOptions

--- hal-wrapper/include/hal-wrapper/peripheral/watch-dog/WindowWatchDogInitOptions.cpp
+++ hal-wrapper/include/hal-wrapper/peripheral/watch-dog/WindowWatchDogInitOptions.cpp
@@ -1,2 +1,25 @@
 #include"WindowWatchDogInitOptions.h"
 
+hal::WindowWatchDogInitOptions::WindowWatchDogInitOptions(WWDG_InitTypeDef const &o)
+{
+	*this = o;
+}
+
+hal::WindowWatchDogInitOptions &hal::WindowWatchDogInitOptions::operator=(WWDG_InitTypeDef const &o)
+{
+	_prescaler = (WindowWatchDogPrescaler)o.Prescaler;
+	_window_upper_bound = o.Window;
+	_counter_reload_value = o.Counter;
+	_early_wakeup_interrupt_mode = (WindowWatchDogEarlyWakeupInterruptMode)o.EWIMode;
+	return *this;
+}
+
+hal::WindowWatchDogInitOptions::operator WWDG_InitTypeDef()
+{
+	WWDG_InitTypeDef o;
+	o.Prescaler = (uint32_t)_prescaler;
+	o.Window = _window_upper_bound;
+	o.Counter = _counter_reload_value;
+	o.EWIMode = (uint32_t)_early_wakeup_interrupt_mode;
+	return o;
+}
--- hal-wrapper/include/hal-wrapper/peripheral/watch-dog/WindowWatchDogInitOptions.h
+++ hal-wrapper/include/hal-wrapper/peripheral/watch-dog/WindowWatchDogInitOptions.h
@@ -17,21 +17,40 @@ namespace hal
 		Disable = WWDG_EWI_DISABLE,
 	};
 
+	/// <summary>
+	///		窗口看门狗初始化选项。
+	/// </summary>
 	class WindowWatchDogInitOptions
 	{
 	public:
+		WindowWatchDogInitOptions() = default;
+		WindowWatchDogInitOptions(WindowWatchDogInitOptions const &o) = default;
+		WindowWatchDogInitOptions(WWDG_InitTypeDef const &o);
+		WindowWatchDogInitOptions &operator=(WindowWatchDogInitOptions const &o) = default;
+		WindowWatchDogInitOptions &operator=(WWDG_InitTypeDef const &o);
+
 		WindowWatchDogPrescaler _prescaler;
 
 		/// <summary>
 		///		窗口上界，范围：[0x40, 0x7F]
 		/// </summary>
-		uint32_t _window;
+		uint32_t _window_upper_bound;
 
 		/// <summary>
-		///		范围：[0x40, 0x7F]
+		///		计数器重载值。
+		///		* 范围：[0x40, 0x7F]
+		///		* 一般要大于 _window_upper_bound。如果小于 _window_upper_bound，
+		///		  那实际的喂狗窗口时间段就会比 _window_upper_bound - 0x40 小，此时
+		///		  _window_upper_bound 虽然比 _counter_reload_value 大，但是没有
+		///		  意义。
 		/// </summary>
-		uint32_t Counter;
+		uint32_t _counter_reload_value;
 
+		/// <summary>
+		///		选择看门狗即将超时时是否触发 “提早唤醒中断”。
+		/// </summary>
 		WindowWatchDogEarlyWakeupInterruptMode _early_wakeup_interrupt_mode;
+
+		operator WWDG_InitTypeDef();
 	};
 }
