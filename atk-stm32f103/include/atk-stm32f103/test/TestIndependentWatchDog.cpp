dc192d3f21db2eee1bdc6ef53332ad91e467c21f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-25 23:52:16 +0800

    BSP_Delayer

--- atk-stm32f103/include/atk-stm32f103/test/TestIndependentWatchDog.cpp
+++ atk-stm32f103/include/atk-stm32f103/test/TestIndependentWatchDog.cpp
@@ -1,11 +1,10 @@
 #include"TestIndependentWatchDog.h"
 #include<atk-stm32f103/bsp.h>
-#include<hal-wrapper/clock/Delayer.h>
 
 void atk::TestIndependentWatchDog()
 {
 	BSP_Initialize();
-	hal::Delayer::Instance().Delay(std::chrono::milliseconds(500));
+	BSP_Delayer().Delay(std::chrono::milliseconds(500));
 	BSP_RedDigitalLed().TurnOn();
 	BSP_IndependentWatchDog().SetWatchDogTimeoutDuration(std::chrono::milliseconds(1000));
 
