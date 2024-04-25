163bb42aec5b247b9dcd076dea8df8a6303aa744
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-26 00:36:18 +0800

    1

--- atk-stm32f103/private_src/Serial.cpp
+++ atk-stm32f103/private_src/Serial.cpp
@@ -27,6 +27,8 @@ void bsp::Serial::SetLength(int64_t value)
 int32_t bsp::Serial::Read(uint8_t *buffer, int32_t offset, int32_t count)
 {
 	// 接收采用循环缓冲区配合接收中断
+	// 没有数据可读就一直等待，直到有数据可读。
+
 	return 0;
 }
 
@@ -38,11 +40,14 @@ void bsp::Serial::Write(uint8_t const *buffer, int32_t offset, int32_t count)
 
 void bsp::Serial::Flush()
 {
+	// 等待 DMA 发送完成
+
 }
 
 void bsp::Serial::Close()
 {
-	// 串口不支持关闭
+	// 串口反初始化
+
 }
 
 int64_t bsp::Serial::Position()
@@ -56,14 +61,20 @@ void bsp::Serial::SetPosition(int64_t value)
 
 void bsp::Serial::Begin(uint32_t baud_rate)
 {
+
 }
 
 int32_t bsp::Serial::AvaliableToRead()
 {
+	// 返回循环缓冲区中的可用数据长度
+
 	return 0;
 }
 
 int32_t bsp::Serial::AvaliableToWrite()
 {
+	/* 返回发送缓冲区剩余的长度。
+	* 这里的缓冲区就是一个简单的数组，满了就调用 DMA 进行发送
+	*/
 	return 0;
 }
