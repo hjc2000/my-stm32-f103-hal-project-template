b80c10ddc2d28f0a0b52cc55ffc5d3ea2df1c400
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-26 00:30:39 +0800

    Read

--- atk-stm32f103/private_src/Serial.h
+++ atk-stm32f103/private_src/Serial.h
@@ -13,6 +13,14 @@ namespace bsp
 		int64_t Length() override;
 		void SetLength(int64_t value) override;
 
+		/// <summary>
+		///		读取前先检查 AvaliableToRead。如果没有数据可读，为了避免 Read
+		///		返回 0，本函数会一直等待，直到 AvaliableToRead 不为 0.
+		/// </summary>
+		/// <param name="buffer"></param>
+		/// <param name="offset"></param>
+		/// <param name="count"></param>
+		/// <returns></returns>
 		int32_t Read(uint8_t *buffer, int32_t offset, int32_t count) override;
 		void Write(uint8_t const *buffer, int32_t offset, int32_t count) override;
 
