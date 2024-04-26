05d9b98a7b38e62b871816b4608fe623b65b36b9
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-26 15:13:33 +0800

    PllInitOptions

--- hal-wrapper/include/hal-wrapper/clock/Osc.cpp
+++ hal-wrapper/include/hal-wrapper/clock/Osc.cpp
@@ -2,23 +2,6 @@
 
 using namespace hal;
 
-PllInitOptions &PllInitOptions::operator=(RCC_PLLInitTypeDef const &value)
-{
-	_state = (PllState)value.PLLState;
-	_clock_source = (PllClockSource)value.PLLSource;
-	_mul = (PllMul)value.PLLMUL;
-	return *this;
-}
-
-PllInitOptions::operator RCC_PLLInitTypeDef() const
-{
-	RCC_PLLInitTypeDef def;
-	def.PLLState = (uint32_t)_state;
-	def.PLLSource = (uint32_t)_clock_source;
-	def.PLLMUL = (uint32_t)_mul;
-	return def;
-}
-
 OscInitOptions &OscInitOptions::operator=(RCC_OscInitTypeDef const &value)
 {
 	_oscillator_type = (OscillatorType)value.OscillatorType;
