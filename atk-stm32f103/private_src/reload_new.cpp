61f8a3fa6f8ef23a7dd5f850496d97467d48f4c0
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-27 22:46:53 +0800

    删除container。如果需要，到时候从video项目里复制过来。

--- atk-stm32f103/private_src/reload_new.cpp
+++ atk-stm32f103/private_src/reload_new.cpp
@@ -3,6 +3,11 @@
 #include <cstddef>      // For size_t
 #include <new>          // For std::bad_alloc
 
+/// <summary>
+///		利用 heap4 实现。
+/// </summary>
+/// <param name="size"></param>
+/// <returns></returns>
 void *operator new(size_t size)
 {
 	void *ptr = pvPortMalloc(size);  // Use FreeRTOS's memory allocation
@@ -14,6 +19,11 @@ void *operator new(size_t size)
 	return ptr;
 }
 
+/// <summary>
+///		利用 heap4 实现。
+/// </summary>
+/// <param name="size"></param>
+/// <returns></returns>
 void *operator new[](size_t size)
 {
 	void *ptr = pvPortMalloc(size);  // Use FreeRTOS's memory allocation
@@ -25,43 +35,81 @@ void *operator new[](size_t size)
 	return ptr;
 }
 
+/// <summary>
+///		利用 heap4 实现。
+/// </summary>
+/// <param name="ptr"></param>
 void operator delete(void *ptr) noexcept
 {
 	vPortFree(ptr);  // Use FreeRTOS's memory deallocation
 }
 
+/// <summary>
+/// 	利用 heap4 实现。
+/// </summary>
+/// <param name="ptr"></param>
 void operator delete[](void *ptr) noexcept
 {
 	vPortFree(ptr);  // Use FreeRTOS's memory deallocation
 }
 
-// Optional: define the nothrow versions of new, which should not throw exceptions
+/// <summary>
+/// 	利用 heap4 实现。
+/// </summary>
+/// <param name="size"></param>
+/// <param name=""></param>
+/// <returns></returns>
 void *operator new(size_t size, const std::nothrow_t &) noexcept
 {
 	return pvPortMalloc(size);
 }
 
+/// <summary>
+/// 	利用 heap4 实现。
+/// </summary>
+/// <param name="size"></param>
+/// <param name=""></param>
+/// <returns></returns>
 void *operator new[](size_t size, const std::nothrow_t &) noexcept
 {
 	return pvPortMalloc(size);
 }
 
+/// <summary>
+/// 	利用 heap4 实现。
+/// </summary>
+/// <param name="ptr"></param>
+/// <param name=""></param>
 void operator delete(void *ptr, const std::nothrow_t &) noexcept
 {
 	vPortFree(ptr);
 }
 
+/// <summary>
+/// 	利用 heap4 实现。
+/// </summary>
+/// <param name="ptr"></param>
+/// <param name=""></param>
 void operator delete[](void *ptr, const std::nothrow_t &) noexcept
 {
 	vPortFree(ptr);
 }
 
-// Optional: define matching delete for exceptions handling in new
+/// <summary>
+/// 	利用 heap4 实现。
+/// </summary>
+/// <param name="ptr"></param>
+/// <param name=""></param>
 void operator delete(void *ptr, size_t) noexcept
 {
 	vPortFree(ptr);
 }
 
+/// <summary>
+/// 	利用 heap4 实现。
+/// </summary>
+/// <param name="ptr"></param>
+/// <param name=""></param>
 void operator delete[](void *ptr, size_t) noexcept
 {
 	vPortFree(ptr);
