dc201b7f601ae37c81942e73f530b7ee5ef5e078
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-28 11:47:05 +0800

    // 这是在中断函数中，禁止使用 Delayer 进行延时。

--- atk-stm32f103/private_src/AtkExtiKey.cpp
+++ atk-stm32f103/private_src/AtkExtiKey.cpp
@@ -7,6 +7,7 @@ ExtiKey0::ExtiKey0()
 {
 	Exti::Instance()._on_exti4_interrupt = [this]()
 	{
+		// 这是在中断函数中，禁止使用 Delayer 进行延时。
 		Systic::NopLoopDelay(std::chrono::milliseconds(20));
 		_is_pressed = !Port().DigitalReadPin(Pin());
 		Exti::Instance().ClearGpioInterruptPending(Pin());
