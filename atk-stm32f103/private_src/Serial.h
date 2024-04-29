38d4fbff7f81c15ccca8fd0a69ea3ab2d7d05d27
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-04-29 22:52:25 +0800

    UART_HandleTypeDef _uart_handle{};   DMA_HandleTypeDef _dma_handle{};

--- atk-stm32f103/private_src/Serial.h
+++ atk-stm32f103/private_src/Serial.h
@@ -23,8 +23,8 @@ namespace atk
 		uint32_t _baud_rate = 115200;
 		uint8_t _receive_buffer[1];
 
-		UART_HandleTypeDef _uart_handle;
-		DMA_HandleTypeDef _dma_handle;
+		UART_HandleTypeDef _uart_handle{};
+		DMA_HandleTypeDef _dma_handle{};
 		USART_TypeDef *_uart_hardware_instance = USART1;
 		DMA_Channel_TypeDef *_dma_channel_hardware_instance = DMA1_Channel4;
 
