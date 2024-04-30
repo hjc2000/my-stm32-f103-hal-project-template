547ff7d0962abb549ffc21216721f50d5ed0b503
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-30 09:27:37 +0800

    Serial

--- atk-stm32f103/private_src/Serial.cpp
+++ atk-stm32f103/private_src/Serial.cpp
@@ -148,7 +148,8 @@ void Serial::SetPosition(int64_t value)
 
 void Serial::Begin(uint32_t baud_rate)
 {
-	/* 先立刻释放一次信号量，等会 Write 方法被调用时直接通过，不被阻塞。
+	/* 
+	* 先立刻释放一次信号量，等会 Write 方法被调用时直接通过，不被阻塞。
 	* 然后在发送完成之前，第二次 Write 就会被阻塞了，这还能防止 Write
 	* 被多线程同时调用。
 	*/
@@ -161,9 +162,12 @@ void Serial::Begin(uint32_t baud_rate)
 	_uart_handle.Instance = _uart_hardware_instance;
 	_uart_handle.Init = options;
 	_uart_handle.MspInitCallback = OnMspInitCallback;
-
-	// 启动
 	HAL_UART_Init(&_uart_handle);
+
+	/* 
+	* HAL_UART_Init 函数会把中断处理函数中回调的函数都设为默认的，所以必须在 HAL_UART_Init
+	* 之后对函数指针赋值。
+	*/
 	_uart_handle.RxCpltCallback = OnReceiveCompleteCallback;
 	_uart_handle.TxCpltCallback = OnSendCompleteCallback;
 
