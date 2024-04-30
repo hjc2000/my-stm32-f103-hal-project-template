1a7f6c86ba158cf80cd9ffd54d29cef8df57ab29
hjc <96368006+hjc2000@users.noreply.github.com>
2024-04-30 13:18:06 +0800

    int _write(int file, char *ptr, int len)

--- atk-stm32f103/private_src/Implement_stdio.cpp
+++ atk-stm32f103/private_src/Implement_stdio.cpp
@@ -4,6 +4,16 @@
 
 extern "C"
 {
+	/// <summary>
+	///		stdio 底层会调用本函数。
+	/// </summary>
+	/// <param name="file">
+	///		文件描述符
+	///		* 对于标准输出，file = 1，对于错误输出，file = 2。
+	/// </param>
+	/// <param name="ptr"></param>
+	/// <param name="len"></param>
+	/// <returns></returns>
 	int _write(int file, char *ptr, int len)
 	{
 		try
