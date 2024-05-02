cd00f02a0c9b6288340512c7f1791077e89903ee
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-02 14:34:07 +0800

    static void Run(std::function<void()> f)

--- atk-stm32f103/private_src/task/Critical.h
+++ atk-stm32f103/private_src/task/Critical.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<FreeRTOS.h>
+#include<functional>
 #include<task.h>
 
 namespace task
@@ -33,6 +34,24 @@ namespace task
 		static void ExitFromISR()
 		{
 			taskEXIT_CRITICAL_FROM_ISR(_isr_enter_ret);
+		}
+
+		/// <summary>
+		///		在非中断中执行临界区任务
+		/// </summary>
+		/// <param name="f"></param>
+		static void Run(std::function<void()> f)
+		{
+			Enter();
+			f();
+			Exit();
+		}
+
+		static void RunFromISR(std::function<void()> f)
+		{
+			EnterFromISR();
+			f();
+			ExitFromISR();
 		}
 	};
 }
