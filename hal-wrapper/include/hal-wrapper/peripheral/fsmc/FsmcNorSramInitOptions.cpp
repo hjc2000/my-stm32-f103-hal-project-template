03f7d2d229f58852bb89944e03785cf26889bca7
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-05 00:50:22 +0800

    class FsmcNorSramInitOptions

--- hal-wrapper/include/hal-wrapper/peripheral/fsmc/FsmcNorSramInitOptions.cpp
+++ hal-wrapper/include/hal-wrapper/peripheral/fsmc/FsmcNorSramInitOptions.cpp
@@ -27,5 +27,19 @@ hal::FsmcNorSramInitOptions &hal::FsmcNorSramInitOptions::operator=(FSMC_NORSRAM
 hal::FsmcNorSramInitOptions::operator FSMC_NORSRAM_InitTypeDef() const
 {
 	FSMC_NORSRAM_InitTypeDef o;
+	o.NSBank = static_cast<uint32_t>(_bank);
+	o.DataAddressMux = static_cast<uint32_t>(_data_address_mux);
+	o.MemoryType = static_cast<uint32_t>(_memory_type);
+	o.MemoryDataWidth = static_cast<uint32_t>(_memory_data_width);
+	o.BurstAccessMode = static_cast<uint32_t>(_burst_access_mode);
+	o.WaitSignalPolarity = static_cast<uint32_t>(_wait_signal_polarity);
+	o.WrapMode = static_cast<uint32_t>(_wrap_mode);
+	o.WaitSignalActive = static_cast<uint32_t>(_wait_signal_active);
+	o.WriteOperation = static_cast<uint32_t>(_write_operation);
+	o.WaitSignal = static_cast<uint32_t>(_wait_signal);
+	o.ExtendedMode = static_cast<uint32_t>(_extended_mode);
+	o.AsynchronousWait = static_cast<uint32_t>(_asynchronous_wait);
+	o.WriteBurst = static_cast<uint32_t>(_write_burst);
+	o.PageSize = static_cast<uint32_t>(_page_size);
 	return o;
 }
