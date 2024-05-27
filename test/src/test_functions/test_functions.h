4bb4d5ba16ceda770a588033a64a58440746fe79
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-28 00:02:11 +0800

    bsp-interface

--- test/src/test_functions/test_functions.h
+++ test/src/test_functions/test_functions.h
@@ -2,7 +2,6 @@
 #include<TestBaseTimer.h>
 #include<TestExtiKey.h>
 #include<TestIndependentWatchDog.h>
-#include<TestJson.h>
 #include<TestKeyScanner.h>
 #include<TestLcd.h>
 #include<TestSerial.h>
