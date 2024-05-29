25f0609e0de8b0695cec65a5934b889c1badabc1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-30 00:16:10 +0800

    class ClockSignal

--- stm32f103-hal-wrapper/include/stm32f103-hal-wrapper/clock/ClockSignal.h
+++ stm32f103-hal-wrapper/include/stm32f103-hal-wrapper/clock/ClockSignal.h
@@ -7,9 +7,6 @@ namespace hal
 	{
 	private:
 		ClockSignal() = delete;
-		ClockSignal(ClockSignal const &o) = delete;
-		ClockSignal(ClockSignal const &&o) = delete;
-		ClockSignal &operator=(ClockSignal const &o) = delete;
 
 	public:
 		/// <summary>
