e180afd8a6b3dd19c449f6b5b82cfff02dfa65bd
hjc2000 <hjc.666@foxmail.com>
2024-05-30 13:17:03 +0800

    class UartConfig

--- atk-stm32f103/private_src/peripheral/serial/UartConfig.cpp
+++ atk-stm32f103/private_src/peripheral/serial/UartConfig.cpp
@@ -2,32 +2,32 @@
 
 using namespace bsp;
 
-bsp::UartConfig::UartConfig(UART_InitTypeDef const &value)
+bsp::UartConfig::UartConfig(UART_InitTypeDef const &o)
 {
-	*this = value;
+	*this = o;
 }
 
-UartConfig &bsp::UartConfig::operator=(UART_InitTypeDef const &value)
+UartConfig &bsp::UartConfig::operator=(UART_InitTypeDef const &o)
 {
-	_baud_rate = value.BaudRate;
-	_word_length = (UartWordLength)value.WordLength;
-	_stop_bit_count = (UartStopBitCount)value.StopBits;
-	_parity = (UartParity)value.Parity;
-	_mode = (UartMode)value.Mode;
-	_hw_flow_ctl = (UartHardwareFlowControl)value.HwFlowCtl;
-	_over_sampling = (UartOverSample)value.OverSampling;
+	_baud_rate = o.BaudRate;
+	_word_length = (UartWordLength)o.WordLength;
+	_stop_bit_count = (UartStopBitCount)o.StopBits;
+	_parity = (UartParity)o.Parity;
+	_mode = (UartMode)o.Mode;
+	_hw_flow_ctl = (UartHardwareFlowControl)o.HwFlowCtl;
+	_over_sampling = (UartOverSample)o.OverSampling;
 	return *this;
 }
 
 bsp::UartConfig::operator UART_InitTypeDef() const
 {
-	UART_InitTypeDef def;
-	def.BaudRate = _baud_rate;
-	def.WordLength = (uint32_t)_word_length;
-	def.StopBits = (uint32_t)_stop_bit_count;
-	def.Parity = (uint32_t)_parity;
-	def.Mode = (uint32_t)_mode;
-	def.HwFlowCtl = (uint32_t)_hw_flow_ctl;
-	def.OverSampling = (uint32_t)_over_sampling;
-	return def;
+	UART_InitTypeDef o;
+	o.BaudRate = _baud_rate;
+	o.WordLength = (uint32_t)_word_length;
+	o.StopBits = (uint32_t)_stop_bit_count;
+	o.Parity = (uint32_t)_parity;
+	o.Mode = (uint32_t)_mode;
+	o.HwFlowCtl = (uint32_t)_hw_flow_ctl;
+	o.OverSampling = (uint32_t)_over_sampling;
+	return o;
 }
--- atk-stm32f103/private_src/peripheral/serial/UartConfig.h
+++ atk-stm32f103/private_src/peripheral/serial/UartConfig.h
@@ -3,93 +3,15 @@
 
 namespace bsp
 {
-	/// <summary>
-	///		一个串行帧有多少位
-	/// </summary>
-	enum class UartWordLength
-	{
-		/// <summary>
-		///		一个串行帧有 8 位。
-		/// </summary>
-		WordLength_8bit = UART_WORDLENGTH_8B,
-
-		/// <summary>
-		///		一个串行帧有 9 位。
-		/// </summary>
-		WordLength_9bit = UART_WORDLENGTH_9B,
-	};
-
-	/// <summary>
-	///		有多少位停止位
-	/// </summary>
-	enum class UartStopBitCount
-	{
-		StopBitCount_1bit = UART_STOPBITS_1,
-		StopBitCount_2bit = UART_STOPBITS_2,
-	};
-
-	/// <summary>
-	///		UART 校验模式
-	/// </summary>
-	enum class UartParity
-	{
-		None = UART_PARITY_NONE,
-		Even = UART_PARITY_EVEN,
-		Odd = UART_PARITY_ODD,
-	};
-
-	/// <summary>
-	///		串口收发模式。
-	/// </summary>
-	enum class UartMode
-	{
-		/// <summary>
-		///		只接收
-		/// </summary>
-		RX = UART_MODE_RX,
-
-		/// <summary>
-		///		只发送
-		/// </summary>
-		TX = UART_MODE_TX,
-
-		/// <summary>
-		///		接收和发送都启用
-		/// </summary>
-		RX_TX = UART_MODE_TX_RX,
-	};
-
-	/// <summary>
-	///		硬件流控
-	/// </summary>
-	enum class UartHardwareFlowControl
-	{
-		None = UART_HWCONTROL_NONE,
-		RTS = UART_HWCONTROL_RTS,
-		CTS = UART_HWCONTROL_CTS,
-		RTS_CTS = UART_HWCONTROL_RTS_CTS,
-	};
-
-	/// <summary>
-	///		过采样倍率
-	/// </summary>
-	enum class UartOverSample
-	{
-		OverSample_16 = UART_OVERSAMPLING_16,
-
-		#if defined(USART_CR1_OVER8)
-		OverSample_8,
-		#endif /* USART_CR1_OVER8 */
-	};
-
 	class UartConfig
 	{
 	public:
 		UartConfig() = default;
-		UartConfig(UartConfig const &value) = default;
-		UartConfig(UART_InitTypeDef const &value);
-		UartConfig &operator=(UartConfig const &value) = default;
-		UartConfig &operator=(UART_InitTypeDef const &value);
+		UartConfig(UartConfig const &o) = default;
+		UartConfig(UART_InitTypeDef const &o);
+		UartConfig &operator=(UartConfig const &o) = default;
+		UartConfig &operator=(UART_InitTypeDef const &o);
+		operator UART_InitTypeDef() const;
 
 		/// <summary>
 		///		波特率
@@ -99,7 +21,32 @@ namespace bsp
 		/// <summary>
 		///		一个串行帧有多少位
 		/// </summary>
+		enum class UartWordLength
+		{
+			/// <summary>
+			///		一个串行帧有 8 位。
+			/// </summary>
+			WordLength_8bit = UART_WORDLENGTH_8B,
+
+			/// <summary>
+			///		一个串行帧有 9 位。
+			/// </summary>
+			WordLength_9bit = UART_WORDLENGTH_9B,
+		};
+
+		/// <summary>
+		///		一个串行帧有多少位
+		/// </summary>
 		UartWordLength _word_length = UartWordLength::WordLength_8bit;
+
+		/// <summary>
+		///		有多少位停止位
+		/// </summary>
+		enum class UartStopBitCount
+		{
+			StopBitCount_1bit = UART_STOPBITS_1,
+			StopBitCount_2bit = UART_STOPBITS_2,
+		};
 
 		/// <summary>
 		///		一个串行帧有多少个停止位
@@ -107,10 +54,41 @@ namespace bsp
 		UartStopBitCount _stop_bit_count = UartStopBitCount::StopBitCount_1bit;
 
 		/// <summary>
+		///		UART 校验模式
+		/// </summary>
+		enum class UartParity
+		{
+			None = UART_PARITY_NONE,
+			Even = UART_PARITY_EVEN,
+			Odd = UART_PARITY_ODD,
+		};
+
+		/// <summary>
 		///		校验模式。
 		///		- 校验位被放到串行帧中除了停止位以外的最高位。
 		/// </summary>
 		UartParity _parity = UartParity::None;
+
+		/// <summary>
+		///		串口收发模式。
+		/// </summary>
+		enum class UartMode
+		{
+			/// <summary>
+			///		只接收
+			/// </summary>
+			RX = UART_MODE_RX,
+
+			/// <summary>
+			///		只发送
+			/// </summary>
+			TX = UART_MODE_TX,
+
+			/// <summary>
+			///		接收和发送都启用
+			/// </summary>
+			RX_TX = UART_MODE_TX_RX,
+		};
 
 		/// <summary>
 		///		收发模式
@@ -120,13 +98,34 @@ namespace bsp
 		/// <summary>
 		///		硬件流控
 		/// </summary>
+		enum class UartHardwareFlowControl
+		{
+			None = UART_HWCONTROL_NONE,
+			RTS = UART_HWCONTROL_RTS,
+			CTS = UART_HWCONTROL_CTS,
+			RTS_CTS = UART_HWCONTROL_RTS_CTS,
+		};
+
+		/// <summary>
+		///		硬件流控
+		/// </summary>
 		UartHardwareFlowControl _hw_flow_ctl = UartHardwareFlowControl::None;
 
 		/// <summary>
 		///		过采样倍率
 		/// </summary>
-		UartOverSample _over_sampling = UartOverSample::OverSample_16;
+		enum class UartOverSample
+		{
+			OverSample_16 = UART_OVERSAMPLING_16,
 
-		operator UART_InitTypeDef() const;
+			#if defined(USART_CR1_OVER8)
+			OverSample_8,
+			#endif /* USART_CR1_OVER8 */
+		};
+
+		/// <summary>
+		///		过采样倍率
+		/// </summary>
+		UartOverSample _over_sampling = UartOverSample::OverSample_16;
 	};
 }
