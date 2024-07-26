d47ed86653a6c8a8c03236f887e23d733d0c37ae
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-27 01:18:06 +0800

    bsp::IInterruptSwitch &DI_InterruptSwitch()

--- stm32f103-hal-wrapper/include/hal-wrapper/di.cpp
+++ stm32f103-hal-wrapper/include/hal-wrapper/di.cpp
@@ -3,40 +3,42 @@
 #include <hal-wrapper/clock/SysTickClock.h>
 #include <hal-wrapper/interrupt/Interrupt.h>
 
+#pragma region DI_SysTick
 bsp::ISysTick &DI_SysTick()
 {
 	return hal::SysTickClock::Instance();
 }
+#pragma endregion
 
 #pragma region DI_InterruptSwitch
-class InterruptSwitch : public bsp::IInterruptSwitch
-{
-public:
-	void DisableInterrupt(uint32_t irq) noexcept override
-	{
-		hal::Interrupt::DisableIRQ(static_cast<IRQn_Type>(irq));
-	}
-
-	void EnableInterrupt(uint32_t irq) noexcept override
-	{
-		hal::Interrupt::EnableIRQ(static_cast<IRQn_Type>(irq));
-	}
-
-	/// @brief 禁止全局中断
-	virtual void DisableGlobalInterrupt() noexcept
-	{
-		__disable_irq();
-	}
-
-	/// @brief 启用全局中断
-	virtual void EnableGlobalInterrupt() noexcept
-	{
-		__enable_irq();
-	}
-};
-
 bsp::IInterruptSwitch &DI_InterruptSwitch()
 {
+	class InterruptSwitch : public bsp::IInterruptSwitch
+	{
+	public:
+		void DisableInterrupt(uint32_t irq) noexcept override
+		{
+			hal::Interrupt::DisableIRQ(static_cast<IRQn_Type>(irq));
+		}
+
+		void EnableInterrupt(uint32_t irq) noexcept override
+		{
+			hal::Interrupt::EnableIRQ(static_cast<IRQn_Type>(irq));
+		}
+
+		/// @brief 禁止全局中断
+		virtual void DisableGlobalInterrupt() noexcept
+		{
+			__disable_irq();
+		}
+
+		/// @brief 启用全局中断
+		virtual void EnableGlobalInterrupt() noexcept
+		{
+			__enable_irq();
+		}
+	};
+
 	static InterruptSwitch o;
 	return o;
 }
