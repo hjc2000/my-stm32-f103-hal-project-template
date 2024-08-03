908f121c3d45ebcfdbd8d23f8dd092c8d18115e0
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-03 15:23:47 +0800

    class RedDigitalLed

--- atk-stm32f103/private_src/DigitalLed.cpp
+++ atk-stm32f103/private_src/DigitalLed.cpp
@@ -12,6 +12,23 @@ static base::Initializer _initializer{
 	}};
 
 #pragma region RedDigitalLed
+bsp::RedDigitalLed::RedDigitalLed()
+{
+	_pin = DI_GpioPinCollection().Get("PB5");
+	if (_pin == nullptr)
+	{
+		throw std::runtime_error{"无法找到 PB5 引脚"};
+	}
+
+	std::shared_ptr<bsp::IGpioPinOptions> options = DICreate_GpioPinOptions();
+	options->SetWorkMode(bsp::IGpioPinWorkMode::Gpio);
+	options->SetDirection(bsp::IGpioPinDirection::Output);
+	options->SetDriver(bsp::IGpioPinDriver::PushPull);
+	options->SetPullMode(bsp::IGpioPinPullMode::PullUp);
+	options->SetSpeedLevel(2);
+	_pin->Open(*options);
+}
+
 void bsp::RedDigitalLed::TurnOn()
 {
 	// GPIO 引脚连接着 LED 的阴极，所以写 0 是打开
@@ -31,6 +48,18 @@ void bsp::RedDigitalLed::Toggle()
 #pragma endregion
 
 #pragma region GreenDigitalLed
+bsp::GreenDigitalLed::GreenDigitalLed()
+{
+	using namespace hal;
+	GpioPortE::Instance().EnableClock();
+	GpioPinConfig gpio_pin_options;
+	gpio_pin_options.SetPin(hal::GpioPinConfig::PinEnum::Pin5);
+	gpio_pin_options.SetMode(hal::GpioPinConfig::ModeOption::Output_PushPull);
+	gpio_pin_options.SetPull(hal::GpioPinConfig::PullOption::PullUp);
+	gpio_pin_options.SetSpeed(hal::GpioPinConfig::SpeedOption::High);
+	GpioPortE::Instance().InitPin(gpio_pin_options);
+}
+
 void bsp::GreenDigitalLed::TurnOn()
 {
 	// GPIO 引脚连接着 LED 的阴极，所以写 0 是打开
--- atk-stm32f103/private_src/DigitalLed.h
+++ atk-stm32f103/private_src/DigitalLed.h
@@ -12,26 +12,11 @@ namespace bsp
 		: public bsp::IDigitalLed
 	{
 	private:
+		RedDigitalLed();
+
 		bsp::IGpioPin *_pin = nullptr;
 
 	public:
-		RedDigitalLed()
-		{
-			_pin = DI_GpioPinCollection().Get("PB5");
-			if (_pin == nullptr)
-			{
-				throw std::runtime_error{"无法找到 PB5 引脚"};
-			}
-
-			std::shared_ptr<bsp::IGpioPinOptions> options = DICreate_GpioPinOptions();
-			options->SetWorkMode(bsp::IGpioPinWorkMode::Gpio);
-			options->SetDirection(bsp::IGpioPinDirection::Output);
-			options->SetDriver(bsp::IGpioPinDriver::PushPull);
-			options->SetPullMode(bsp::IGpioPinPullMode::PullUp);
-			options->SetSpeedLevel(2);
-			_pin->Open(*options);
-		}
-
 		static RedDigitalLed &Instance()
 		{
 			static RedDigitalLed led;
@@ -44,21 +29,13 @@ namespace bsp
 	};
 
 	/// @brief 绿色 LED
-	class GreenDigitalLed : public bsp::IDigitalLed
+	class GreenDigitalLed
+		: public bsp::IDigitalLed
 	{
-	public:
-		GreenDigitalLed()
-		{
-			using namespace hal;
-			GpioPortE::Instance().EnableClock();
-			GpioPinConfig gpio_pin_options;
-			gpio_pin_options.SetPin(hal::GpioPinConfig::PinEnum::Pin5);
-			gpio_pin_options.SetMode(hal::GpioPinConfig::ModeOption::Output_PushPull);
-			gpio_pin_options.SetPull(hal::GpioPinConfig::PullOption::PullUp);
-			gpio_pin_options.SetSpeed(hal::GpioPinConfig::SpeedOption::High);
-			GpioPortE::Instance().InitPin(gpio_pin_options);
-		}
+	private:
+		GreenDigitalLed();
 
+	public:
 		static GreenDigitalLed &Instance()
 		{
 			static GreenDigitalLed led;
