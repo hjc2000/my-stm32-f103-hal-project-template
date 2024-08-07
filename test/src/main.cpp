1874fa1eb78a5ee59d5be3d2359d7b8f796cc7a8
hjc2000 <hjc.666@foxmail.com>
2024-08-07 08:41:56 +0800

    base::Initializer::Initialize();

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,4 +1,6 @@
+#include <base/Initializer.h>
 #include <base/math/Fraction.h>
+#include <bsp-interface/di.h>
 #include <bsp-interface/test/TestIndependentWatchDog.h>
 #include <bsp-interface/test/TestLcd.h>
 #include <bsp-interface/test/TestSerial.h>
@@ -12,41 +14,39 @@ using namespace bsp;
 
 int main(void)
 {
-	while (true)
+	try
 	{
-		try
-		{
-			BSP::Initialize();
+		BSP::Initialize();
+		base::Initializer::Initialize();
 
-			std::shared_ptr<task::Task> lvgl_init_task = task::Task::Create(
-				[]()
-				{
-					// bsp::TestSerial();
-					TestKeyScanner();
-					// TestLcd();
-					// TestIndependentWatchDog();
-					// TestExtiKey();
-				},
-				512);
+		std::shared_ptr<task::Task> lvgl_init_task = task::Task::Create(
+			[]()
+			{
+				// bsp::TestSerial();
+				TestKeyScanner();
+				// TestLcd();
+				// TestIndependentWatchDog();
+				// TestExtiKey();
+			},
+			512);
 
-			vTaskStartScheduler();
-		}
-		catch (std::exception const &e)
+		vTaskStartScheduler();
+	}
+	catch (std::exception const &e)
+	{
+		DI_RedDigitalLed().TurnOff();
+		BSP::Console().WriteError("main 函数中捕获到逃逸的 std::exception。");
+		BSP::Console().WriteError(e.what());
+		while (true)
 		{
-			DI_RedDigitalLed().TurnOff();
-			BSP::Console().WriteError("main 函数中捕获到逃逸的 std::exception。");
-			BSP::Console().WriteError(e.what());
-			while (true)
-			{
-			}
 		}
-		catch (...)
+	}
+	catch (...)
+	{
+		DI_RedDigitalLed().TurnOff();
+		BSP::Console().WriteError("main 函数中捕获到逃逸的未知类型的异常。");
+		while (true)
 		{
-			DI_RedDigitalLed().TurnOff();
-			BSP::Console().WriteError("main 函数中捕获到逃逸的未知类型的异常。");
-			while (true)
-			{
-			}
 		}
 	}
 }
