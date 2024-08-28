ae3a1f5f8ccae142cfa382c17fb006ccc0fc9c2f
hjc2000 <hjc.666@foxmail.com>
2024-08-28 15:59:36 +0800

    base::IReadOnlyCollection<std::string, bsp::IDmaChannel *> &DI_DmaChannel()

--- stm32f103-hal-wrapper/include/hal-wrapper/peripheral/serial/Dma1Channel5.h
+++ stm32f103-hal-wrapper/include/hal-wrapper/peripheral/serial/Dma1Channel5.h
@@ -12,6 +12,12 @@ namespace bsp
         bool _is_open = false;
 
     public:
+        static Dma1Channel5 &Instance()
+        {
+            static Dma1Channel5 o;
+            return o;
+        }
+
         std::string Name() const override;
         void Open(bsp::IDmaOptions const &options) override;
 
--- stm32f103-hal-wrapper/include/hal-wrapper/peripheral/serial/di_dma.cpp
+++ stm32f103-hal-wrapper/include/hal-wrapper/peripheral/serial/di_dma.cpp
@@ -1,8 +1,66 @@
 #include "Dma1Channel5.h"
 #include "DmaOptions.h"
+#include <base/container/StdMapValuesEnumerator.h>
+#include <base/Initializer.h>
 #include <bsp-interface/di/dma.h>
+#include <map>
+
+base::Initializer _initializer{
+    []()
+    {
+        DI_DmaChannel();
+    }};
 
 std::shared_ptr<bsp::IDmaOptions> DICreate_DmaOptions()
 {
     return std::shared_ptr<bsp::IDmaOptions>{new bsp::DmaOptions{}};
+}
+
+class Collection :
+    public base::IReadOnlyCollection<std::string, bsp::IDmaChannel *>
+{
+private:
+    std::map<std::string, bsp::IDmaChannel *> _map;
+
+    void Add(bsp::IDmaChannel *o)
+    {
+        _map[o->Name()] = o;
+    }
+
+public:
+    Collection()
+    {
+        Add(&bsp::Dma1Channel5::Instance());
+    }
+
+    virtual int Count() const override
+    {
+        return _map.size();
+    }
+
+    virtual bsp::IDmaChannel *Get(std::string key) const override
+    {
+        auto it = _map.find(key);
+        if (it == _map.end())
+        {
+            throw std::runtime_error{"找不到此 DMA"};
+        }
+
+        return it->second;
+    }
+
+    std::shared_ptr<base::IEnumerator<bsp::IDmaChannel *>> GetEnumerator() override
+    {
+        return std::shared_ptr<base::IEnumerator<bsp::IDmaChannel *>>{
+            new base::StdMapValuesEnumerator<std::string, bsp::IDmaChannel *>{
+                &_map,
+            },
+        };
+    }
+};
+
+base::IReadOnlyCollection<std::string, bsp::IDmaChannel *> &DI_DmaChannel()
+{
+    static Collection o;
+    return o;
 }
