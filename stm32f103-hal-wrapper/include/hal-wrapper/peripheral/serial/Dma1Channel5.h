7ef39e9bd05a170a10f0b7b1ff771f64b08b02e8
hjc2000 <hjc.666@foxmail.com>
2024-08-28 15:50:13 +0800

    void Close() override

--- stm32f103-hal-wrapper/include/hal-wrapper/peripheral/serial/Dma1Channel5.h
+++ stm32f103-hal-wrapper/include/hal-wrapper/peripheral/serial/Dma1Channel5.h
@@ -1,16 +1,35 @@
 #pragma once
 #include <bsp-interface/dma/IDmaChannel.h>
+#include <hal.h>
 
 namespace bsp
 {
-    class Dma1Channel5
+    class Dma1Channel5 :
+        public bsp::IDmaChannel
     {
+    private:
+        DMA_HandleTypeDef _handle{};
+        bool _is_open = false;
+
     public:
-        virtual std::string Name() = 0;
-        virtual void Open(bsp::IDmaOptions const &options) = 0;
+        std::string Name() const override;
+        void Open(bsp::IDmaOptions const &options) override;
+
+        bool IsOpen() const override
+        {
+            return _is_open;
+        }
+
+        void Close() override
+        {
+            _is_open = false;
+        }
 
         /// @brief 返回底层的句柄。
         /// @return
-        virtual void *Handle() = 0;
+        virtual void *Handle() override
+        {
+            return &_handle;
+        }
     };
 } // namespace bsp
