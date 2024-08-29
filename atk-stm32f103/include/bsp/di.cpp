05f4e30512a995a7c409678d08d56a3a564e70cb
hjc2000 <hjc.666@foxmail.com>
2024-08-29 15:11:29 +0800

    di/key.cpp

--- atk-stm32f103/include/bsp/di.cpp
+++ atk-stm32f103/include/bsp/di.cpp
@@ -1,10 +1,7 @@
-#include <AtkKey.h>
 #include <base/container/Collection.h>
 #include <base/Initializer.h>
 #include <base/RentedPtrFactory.h>
-#include <bsp-interface/di/key.h>
 #include <bsp-interface/di/led.h>
-#include <bsp-interface/key/KeyScanner.h>
 #include <bsp/bsp.h>
 #include <DigitalLed.h>
 #include <Lcd.h>
@@ -14,43 +11,8 @@ using namespace bsp;
 static base::Initializer _initializer{
     []()
     {
-        DI_KeyCollection();
-        DI_KeyScanner();
         DI_DigitalLedCollection();
     }};
-
-#pragma region DI_KeyScanner
-
-base::ICollection<std::string, bsp::IKey *> const &DI_KeyCollection()
-{
-    class Collection
-    {
-    public:
-        Collection()
-        {
-            AddKey(&Key0::Instance());
-            AddKey(&Key1::Instance());
-        }
-
-        base::Collection<std::string, bsp::IKey *> _collection;
-
-        void AddKey(bsp::IKey *key)
-        {
-            _collection.Put(key->KeyName(), key);
-        }
-    };
-
-    static Collection o;
-    return o._collection;
-}
-
-bsp::IKeyScanner &DI_KeyScanner()
-{
-    static bsp::KeyScanner key_scanner{};
-    return key_scanner;
-}
-
-#pragma endregion
 
 #pragma region LED
 
