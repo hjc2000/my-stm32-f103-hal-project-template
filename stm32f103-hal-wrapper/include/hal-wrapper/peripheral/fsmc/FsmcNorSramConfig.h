41dae3ff2fcd8de6b43acaa459b50d55b88d455d
hjc2000 <hjc.666@foxmail.com>
2024-08-29 09:28:26 +0800

    格式化

--- stm32f103-hal-wrapper/include/hal-wrapper/peripheral/fsmc/FsmcNorSramConfig.h
+++ stm32f103-hal-wrapper/include/hal-wrapper/peripheral/fsmc/FsmcNorSramConfig.h
@@ -1,239 +1,253 @@
 #pragma once
-#include<base/HandleWrapper.h>
-#include<hal.h>
+#include <base/HandleWrapper.h>
+#include <hal.h>
 
 namespace hal
 {
-	class FsmcNorSramConfig :
-		public base::HandleWrapper<FSMC_NORSRAM_InitTypeDef>
-	{
-	private:
-		FSMC_NORSRAM_InitTypeDef _handle { };
+    class FsmcNorSramConfig :
+        public base::HandleWrapper<FSMC_NORSRAM_InitTypeDef>
+    {
+    private:
+        FSMC_NORSRAM_InitTypeDef _handle{};
 
-	public:
-		FSMC_NORSRAM_InitTypeDef &Handle() override
-		{
-			return _handle;
-		}
+    public:
+        FSMC_NORSRAM_InitTypeDef &Handle() override
+        {
+            return _handle;
+        }
 
-		enum class BankEnum
-		{
-			Bank1 = FSMC_NORSRAM_BANK1,
-			Bank2 = FSMC_NORSRAM_BANK2,
-			Bank3 = FSMC_NORSRAM_BANK3,
-			Bank4 = FSMC_NORSRAM_BANK4,
-		};
+        enum class BankEnum
+        {
+            Bank1 = FSMC_NORSRAM_BANK1,
+            Bank2 = FSMC_NORSRAM_BANK2,
+            Bank3 = FSMC_NORSRAM_BANK3,
+            Bank4 = FSMC_NORSRAM_BANK4,
+        };
 
-		BankEnum Bank() const
-		{
-			return static_cast<BankEnum>(_handle.NSBank);
-		}
-		void SetBank(BankEnum value)
-		{
-			_handle.NSBank = static_cast<uint32_t>(value);
-		}
+        BankEnum Bank() const
+        {
+            return static_cast<BankEnum>(_handle.NSBank);
+        }
 
-		enum class DataAddressMuxOption
-		{
-			Disable = FSMC_DATA_ADDRESS_MUX_DISABLE,
-			Enable = FSMC_DATA_ADDRESS_MUX_ENABLE,
-		};
+        void SetBank(BankEnum value)
+        {
+            _handle.NSBank = static_cast<uint32_t>(value);
+        }
 
-		/// <summary>
-		///		地址总线和数据总线是否复用，即数据和地址全部通过 FSMC 的数据总线进行传输。
-		/// </summary>
-		DataAddressMuxOption DataAddressMux() const
-		{
-			return static_cast<DataAddressMuxOption>(_handle.DataAddressMux);
-		}
-		void SetDataAddressMux(DataAddressMuxOption value)
-		{
-			_handle.DataAddressMux = static_cast<uint32_t>(value);
-		}
+        enum class DataAddressMuxOption
+        {
+            Disable = FSMC_DATA_ADDRESS_MUX_DISABLE,
+            Enable = FSMC_DATA_ADDRESS_MUX_ENABLE,
+        };
 
-		enum class MemoryTypeEnum
-		{
-			SRSM = FSMC_MEMORY_TYPE_SRAM,
-			PSRAM = FSMC_MEMORY_TYPE_PSRAM,
-			NOR = FSMC_MEMORY_TYPE_NOR,
-		};
+        /// <summary>
+        ///		地址总线和数据总线是否复用，即数据和地址全部通过 FSMC 的数据总线进行传输。
+        /// </summary>
+        DataAddressMuxOption DataAddressMux() const
+        {
+            return static_cast<DataAddressMuxOption>(_handle.DataAddressMux);
+        }
 
-		MemoryTypeEnum MemoryType() const
-		{
-			return static_cast<MemoryTypeEnum>(_handle.MemoryType);
-		}
-		void SetMemoryType(MemoryTypeEnum value)
-		{
-			_handle.MemoryType = static_cast<uint32_t>(value);
-		}
+        void SetDataAddressMux(DataAddressMuxOption value)
+        {
+            _handle.DataAddressMux = static_cast<uint32_t>(value);
+        }
 
-		enum class MemoryDataWidthOption
-		{
-			Width8 = FSMC_NORSRAM_MEM_BUS_WIDTH_8,
-			Width16 = FSMC_NORSRAM_MEM_BUS_WIDTH_16,
-			Width32 = FSMC_NORSRAM_MEM_BUS_WIDTH_32,
-		};
+        enum class MemoryTypeEnum
+        {
+            SRSM = FSMC_MEMORY_TYPE_SRAM,
+            PSRAM = FSMC_MEMORY_TYPE_PSRAM,
+            NOR = FSMC_MEMORY_TYPE_NOR,
+        };
 
-		MemoryDataWidthOption MemoryDataWidth() const
-		{
-			return static_cast<MemoryDataWidthOption>(_handle.MemoryDataWidth);
-		}
-		void SetMemoryDataWidth(MemoryDataWidthOption value)
-		{
-			_handle.MemoryDataWidth = static_cast<uint32_t>(value);
-		}
+        MemoryTypeEnum MemoryType() const
+        {
+            return static_cast<MemoryTypeEnum>(_handle.MemoryType);
+        }
 
-		enum class BurstAccessModeOption
-		{
-			Disable = FSMC_BURST_ACCESS_MODE_DISABLE,
-			Enable = FSMC_BURST_ACCESS_MODE_ENABLE,
-		};
+        void SetMemoryType(MemoryTypeEnum value)
+        {
+            _handle.MemoryType = static_cast<uint32_t>(value);
+        }
 
-		BurstAccessModeOption BurstAccessMode() const
-		{
-			return static_cast<BurstAccessModeOption>(_handle.BurstAccessMode);
-		}
-		void SetBurstAccessMode(BurstAccessModeOption value)
-		{
-			_handle.BurstAccessMode = static_cast<uint32_t>(value);
-		}
+        enum class MemoryDataWidthOption
+        {
+            Width8 = FSMC_NORSRAM_MEM_BUS_WIDTH_8,
+            Width16 = FSMC_NORSRAM_MEM_BUS_WIDTH_16,
+            Width32 = FSMC_NORSRAM_MEM_BUS_WIDTH_32,
+        };
 
-		enum class WaitSignalPolarityOption
-		{
-			Low = FSMC_WAIT_SIGNAL_POLARITY_LOW,
-			High = FSMC_WAIT_SIGNAL_POLARITY_HIGH,
-		};
+        MemoryDataWidthOption MemoryDataWidth() const
+        {
+            return static_cast<MemoryDataWidthOption>(_handle.MemoryDataWidth);
+        }
 
-		WaitSignalPolarityOption WaitSignalPolarity() const
-		{
-			return static_cast<WaitSignalPolarityOption>(_handle.WaitSignalPolarity);
-		}
-		void SetWaitSignalPolarity(WaitSignalPolarityOption value)
-		{
-			_handle.WaitSignalPolarity = static_cast<uint32_t>(value);
-		}
+        void SetMemoryDataWidth(MemoryDataWidthOption value)
+        {
+            _handle.MemoryDataWidth = static_cast<uint32_t>(value);
+        }
 
-		enum class WrapModeOption
-		{
-			Disable = FSMC_WRAP_MODE_DISABLE,
-			Enable = FSMC_WRAP_MODE_ENABLE,
-		};
+        enum class BurstAccessModeOption
+        {
+            Disable = FSMC_BURST_ACCESS_MODE_DISABLE,
+            Enable = FSMC_BURST_ACCESS_MODE_ENABLE,
+        };
 
-		WrapModeOption WrapMode() const
-		{
-			return static_cast<WrapModeOption>(_handle.WrapMode);
-		}
-		void SetWrapMode(WrapModeOption value)
-		{
-			_handle.WrapMode = static_cast<uint32_t>(value);
-		}
+        BurstAccessModeOption BurstAccessMode() const
+        {
+            return static_cast<BurstAccessModeOption>(_handle.BurstAccessMode);
+        }
 
-		enum class WaitSignalActiveOption
-		{
-			BeforeWs = FSMC_WAIT_TIMING_BEFORE_WS,
-			DuringWs = FSMC_WAIT_TIMING_DURING_WS,
-		};
+        void SetBurstAccessMode(BurstAccessModeOption value)
+        {
+            _handle.BurstAccessMode = static_cast<uint32_t>(value);
+        }
 
-		WaitSignalActiveOption WaitSignalActive() const
-		{
-			return static_cast<WaitSignalActiveOption>(_handle.WaitSignalActive);
-		}
-		void SetWaitSignalActive(WaitSignalActiveOption value)
-		{
-			_handle.WaitSignalActive = static_cast<uint32_t>(value);
-		}
+        enum class WaitSignalPolarityOption
+        {
+            Low = FSMC_WAIT_SIGNAL_POLARITY_LOW,
+            High = FSMC_WAIT_SIGNAL_POLARITY_HIGH,
+        };
 
-		enum class WriteOperationOption
-		{
-			Disable = FSMC_WRITE_OPERATION_DISABLE,
-			Enable = FSMC_WRITE_OPERATION_ENABLE,
-		};
+        WaitSignalPolarityOption WaitSignalPolarity() const
+        {
+            return static_cast<WaitSignalPolarityOption>(_handle.WaitSignalPolarity);
+        }
 
-		WriteOperationOption WriteOperation() const
-		{
-			return static_cast<WriteOperationOption>(_handle.WriteOperation);
-		}
-		void SetWriteOperation(WriteOperationOption value)
-		{
-			_handle.WriteOperation = static_cast<uint32_t>(value);
-		}
+        void SetWaitSignalPolarity(WaitSignalPolarityOption value)
+        {
+            _handle.WaitSignalPolarity = static_cast<uint32_t>(value);
+        }
 
-		enum class WaitSignalOption
-		{
-			Disable = FSMC_WAIT_SIGNAL_DISABLE,
-			Enable = FSMC_WAIT_SIGNAL_ENABLE,
-		};
+        enum class WrapModeOption
+        {
+            Disable = FSMC_WRAP_MODE_DISABLE,
+            Enable = FSMC_WRAP_MODE_ENABLE,
+        };
 
-		WaitSignalOption WaitSignal() const
-		{
-			return static_cast<WaitSignalOption>(_handle.WaitSignal);
-		}
-		void SetWaitSignal(WaitSignalOption value)
-		{
-			_handle.WaitSignal = static_cast<uint32_t>(value);
-		}
+        WrapModeOption WrapMode() const
+        {
+            return static_cast<WrapModeOption>(_handle.WrapMode);
+        }
 
-		enum class ExtendedModeOption
-		{
-			Disable = FSMC_EXTENDED_MODE_DISABLE,
-			Enable = FSMC_EXTENDED_MODE_ENABLE,
-		};
+        void SetWrapMode(WrapModeOption value)
+        {
+            _handle.WrapMode = static_cast<uint32_t>(value);
+        }
 
-		ExtendedModeOption ExtendedMode() const
-		{
-			return static_cast<ExtendedModeOption>(_handle.ExtendedMode);
-		}
-		void SetExtendedMode(ExtendedModeOption value)
-		{
-			_handle.ExtendedMode = static_cast<uint32_t>(value);
-		}
+        enum class WaitSignalActiveOption
+        {
+            BeforeWs = FSMC_WAIT_TIMING_BEFORE_WS,
+            DuringWs = FSMC_WAIT_TIMING_DURING_WS,
+        };
 
-		enum class AsynchronousWaitOption
-		{
-			Disable = FSMC_ASYNCHRONOUS_WAIT_DISABLE,
-			Enable = FSMC_ASYNCHRONOUS_WAIT_ENABLE,
-		};
+        WaitSignalActiveOption WaitSignalActive() const
+        {
+            return static_cast<WaitSignalActiveOption>(_handle.WaitSignalActive);
+        }
 
-		AsynchronousWaitOption AsynchronousWait() const
-		{
-			return static_cast<AsynchronousWaitOption>(_handle.AsynchronousWait);
-		}
-		void SetAsynchronousWait(AsynchronousWaitOption value)
-		{
-			_handle.AsynchronousWait = static_cast<uint32_t>(value);
-		}
+        void SetWaitSignalActive(WaitSignalActiveOption value)
+        {
+            _handle.WaitSignalActive = static_cast<uint32_t>(value);
+        }
 
-		enum class WriteBurstOption
-		{
-			Disable = FSMC_WRITE_BURST_DISABLE,
-			Enable = FSMC_WRITE_BURST_ENABLE,
-		};
+        enum class WriteOperationOption
+        {
+            Disable = FSMC_WRITE_OPERATION_DISABLE,
+            Enable = FSMC_WRITE_OPERATION_ENABLE,
+        };
 
-		WriteBurstOption WriteBurst() const
-		{
-			return static_cast<WriteBurstOption>(_handle.WriteBurst);
-		}
-		void SetWriteBurst(WriteBurstOption value)
-		{
-			_handle.WriteBurst = static_cast<uint32_t>(value);
-		}
+        WriteOperationOption WriteOperation() const
+        {
+            return static_cast<WriteOperationOption>(_handle.WriteOperation);
+        }
 
-		enum class PageSizeOption
-		{
-			SizeNone = FSMC_PAGE_SIZE_NONE,
-			Size128 = FSMC_PAGE_SIZE_128,
-			Size256 = FSMC_PAGE_SIZE_256,
-			Size512 = FSMC_PAGE_SIZE_512,
-			Size1024 = FSMC_PAGE_SIZE_1024,
-		};
+        void SetWriteOperation(WriteOperationOption value)
+        {
+            _handle.WriteOperation = static_cast<uint32_t>(value);
+        }
 
-		PageSizeOption PageSize() const
-		{
-			return static_cast<PageSizeOption>(_handle.PageSize);
-		}
-		void SetPageSize(PageSizeOption value)
-		{
-			_handle.PageSize = static_cast<uint32_t>(value);
-		}
-	};
-}
+        enum class WaitSignalOption
+        {
+            Disable = FSMC_WAIT_SIGNAL_DISABLE,
+            Enable = FSMC_WAIT_SIGNAL_ENABLE,
+        };
+
+        WaitSignalOption WaitSignal() const
+        {
+            return static_cast<WaitSignalOption>(_handle.WaitSignal);
+        }
+
+        void SetWaitSignal(WaitSignalOption value)
+        {
+            _handle.WaitSignal = static_cast<uint32_t>(value);
+        }
+
+        enum class ExtendedModeOption
+        {
+            Disable = FSMC_EXTENDED_MODE_DISABLE,
+            Enable = FSMC_EXTENDED_MODE_ENABLE,
+        };
+
+        ExtendedModeOption ExtendedMode() const
+        {
+            return static_cast<ExtendedModeOption>(_handle.ExtendedMode);
+        }
+
+        void SetExtendedMode(ExtendedModeOption value)
+        {
+            _handle.ExtendedMode = static_cast<uint32_t>(value);
+        }
+
+        enum class AsynchronousWaitOption
+        {
+            Disable = FSMC_ASYNCHRONOUS_WAIT_DISABLE,
+            Enable = FSMC_ASYNCHRONOUS_WAIT_ENABLE,
+        };
+
+        AsynchronousWaitOption AsynchronousWait() const
+        {
+            return static_cast<AsynchronousWaitOption>(_handle.AsynchronousWait);
+        }
+
+        void SetAsynchronousWait(AsynchronousWaitOption value)
+        {
+            _handle.AsynchronousWait = static_cast<uint32_t>(value);
+        }
+
+        enum class WriteBurstOption
+        {
+            Disable = FSMC_WRITE_BURST_DISABLE,
+            Enable = FSMC_WRITE_BURST_ENABLE,
+        };
+
+        WriteBurstOption WriteBurst() const
+        {
+            return static_cast<WriteBurstOption>(_handle.WriteBurst);
+        }
+
+        void SetWriteBurst(WriteBurstOption value)
+        {
+            _handle.WriteBurst = static_cast<uint32_t>(value);
+        }
+
+        enum class PageSizeOption
+        {
+            SizeNone = FSMC_PAGE_SIZE_NONE,
+            Size128 = FSMC_PAGE_SIZE_128,
+            Size256 = FSMC_PAGE_SIZE_256,
+            Size512 = FSMC_PAGE_SIZE_512,
+            Size1024 = FSMC_PAGE_SIZE_1024,
+        };
+
+        PageSizeOption PageSize() const
+        {
+            return static_cast<PageSizeOption>(_handle.PageSize);
+        }
+
+        void SetPageSize(PageSizeOption value)
+        {
+            _handle.PageSize = static_cast<uint32_t>(value);
+        }
+    };
+} // namespace hal
--- stm32f103-hal-wrapper/include/hal-wrapper/peripheral/fsmc/FsmcNorSramTiming.h
+++ stm32f103-hal-wrapper/include/hal-wrapper/peripheral/fsmc/FsmcNorSramTiming.h
@@ -1,134 +1,141 @@
 #pragma once
-#include<base/HandleWrapper.h>
-#include<hal.h>
+#include <base/HandleWrapper.h>
+#include <hal.h>
 
 namespace hal
 {
-	class FsmcNorSramTiming :
-		public base::HandleWrapper<FSMC_NORSRAM_TimingTypeDef>
-	{
-	private:
-		FSMC_NORSRAM_TimingTypeDef _handle { };
+    class FsmcNorSramTiming :
+        public base::HandleWrapper<FSMC_NORSRAM_TimingTypeDef>
+    {
+    private:
+        FSMC_NORSRAM_TimingTypeDef _handle{};
 
-	public:
-		FSMC_NORSRAM_TimingTypeDef &Handle() override
-		{
-			return _handle;
-		}
+    public:
+        FSMC_NORSRAM_TimingTypeDef &Handle() override
+        {
+            return _handle;
+        }
 
-		/// <summary>
-		///		地址建立时间。
-		///		* 单位：HCLK 的周期数
-		///		* 取值范围：[0, 15]
-		///		* 同步模式下本参数不被使用。
-		///		* 实际值会比这里设置的值多 1 个 HCLK 周期。
-		/// </summary>
-		/// <remark>
-		///		FSMC 在地址总线上输出数据后，因为线路和储存器的输入端的电容效应或其他原因，
-		///		要等一段时间后才能作用到开关管上，作用到开关管上后还要等载流子运动，所以地址
-		///		总线上输出信号后要等一段时间后才能将 WR 或 RD 拉低，进入读写操作的准备阶段
-		///		这个时间就是地址建立时间。
-		/// </remark>
-		uint32_t AddressSetupTime() const
-		{
-			return _handle.AddressSetupTime;
-		}
-		void SetAddressSetupTime(uint32_t value)
-		{
-			_handle.AddressSetupTime = value;
-		}
+        /// <summary>
+        ///		地址建立时间。
+        ///		* 单位：HCLK 的周期数
+        ///		* 取值范围：[0, 15]
+        ///		* 同步模式下本参数不被使用。
+        ///		* 实际值会比这里设置的值多 1 个 HCLK 周期。
+        /// </summary>
+        /// <remark>
+        ///		FSMC 在地址总线上输出数据后，因为线路和储存器的输入端的电容效应或其他原因，
+        ///		要等一段时间后才能作用到开关管上，作用到开关管上后还要等载流子运动，所以地址
+        ///		总线上输出信号后要等一段时间后才能将 WR 或 RD 拉低，进入读写操作的准备阶段
+        ///		这个时间就是地址建立时间。
+        /// </remark>
+        uint32_t AddressSetupTime() const
+        {
+            return _handle.AddressSetupTime;
+        }
 
-		/// <summary>
-		///		地址保持时间。
-		///		* 单位：HCLK 的周期数
-		///		* 取值范围：[0, 15]
-		///		* 同步模式下本参数不被使用。
-		/// </summary>
-		uint32_t AddressHoldTime() const
-		{
-			return _handle.AddressHoldTime;
-		}
-		void SetAddressHoldTime(uint32_t value)
-		{
-			_handle.AddressHoldTime = value;
-		}
+        void SetAddressSetupTime(uint32_t value)
+        {
+            _handle.AddressSetupTime = value;
+        }
 
-		/// <summary>
-		///		数据建立时间。
-		///		* 单位：HCLK 的周期数
-		///		* 取值范围：[1, 255]
-		///		* 实际值会比这里设置的值多 1 个 HCLK 周期。
-		/// </summary>
-		/// <remark>
-		///		读取模式下，FSMC 将 RD 拉低后，要等一段时间后储存器才能让数据总线上有
-		///		稳定的，可采样的信号。这个时间就是数据建立时间。
-		/// </remark>
-		/// <remark>
-		///		写模式下，FSMC 将数据输出到数据总线上同时将 WR 拉低后，因为电容效应和载流子
-		///		移动需要时间等原因，还要等一段时间后才能将 WR 拉高，让储存器进行锁存。
-		///		这个时间就是数据建立时间。
-		///		
-		///		WR 拉高后，数据总线上的数据还会被 FSMC 保持 1 个 HCLK 周期。
-		/// </remark>
-		uint32_t DataSetupTime() const
-		{
-			return _handle.DataSetupTime;
-		}
-		void SetDataSetupTime(uint32_t value)
-		{
-			_handle.DataSetupTime = value;
-		}
+        /// <summary>
+        ///		地址保持时间。
+        ///		* 单位：HCLK 的周期数
+        ///		* 取值范围：[0, 15]
+        ///		* 同步模式下本参数不被使用。
+        /// </summary>
+        uint32_t AddressHoldTime() const
+        {
+            return _handle.AddressHoldTime;
+        }
 
-		/// <summary>
-		///		* 单位：HCLK 的周期数
-		///		* 取值范围：[0, 15]
-		/// </summary>
-		uint32_t BusTurnAroundDuration() const
-		{
-			return _handle.BusTurnAroundDuration;
-		}
-		void SetBusTurnAroundDuration(uint32_t value)
-		{
-			_handle.BusTurnAroundDuration = value;
-		}
+        void SetAddressHoldTime(uint32_t value)
+        {
+            _handle.AddressHoldTime = value;
+        }
 
-		/// <summary>
-		///		* 单位：HCLK 的周期数
-		///		* 取值范围：[2, 16]
-		/// </summary>
-		uint32_t ClockDivision() const
-		{
-			return _handle.CLKDivision;
-		}
-		void SetClockDivision(uint32_t value)
-		{
-			_handle.CLKDivision = value;
-		}
+        /// <summary>
+        ///		数据建立时间。
+        ///		* 单位：HCLK 的周期数
+        ///		* 取值范围：[1, 255]
+        ///		* 实际值会比这里设置的值多 1 个 HCLK 周期。
+        /// </summary>
+        /// <remark>
+        ///		读取模式下，FSMC 将 RD 拉低后，要等一段时间后储存器才能让数据总线上有
+        ///		稳定的，可采样的信号。这个时间就是数据建立时间。
+        /// </remark>
+        /// <remark>
+        ///		写模式下，FSMC 将数据输出到数据总线上同时将 WR 拉低后，因为电容效应和载流子
+        ///		移动需要时间等原因，还要等一段时间后才能将 WR 拉高，让储存器进行锁存。
+        ///		这个时间就是数据建立时间。
+        ///
+        ///		WR 拉高后，数据总线上的数据还会被 FSMC 保持 1 个 HCLK 周期。
+        /// </remark>
+        uint32_t DataSetupTime() const
+        {
+            return _handle.DataSetupTime;
+        }
 
-		uint32_t DataLatency() const
-		{
-			return _handle.DataLatency;
-		}
-		void SetDataLatency(uint32_t value)
-		{
-			_handle.DataLatency = value;
-		}
+        void SetDataSetupTime(uint32_t value)
+        {
+            _handle.DataSetupTime = value;
+        }
 
-		enum class AccessModeOption
-		{
-			ModeA = FSMC_ACCESS_MODE_A,
-			ModeB = FSMC_ACCESS_MODE_B,
-			ModeC = FSMC_ACCESS_MODE_C,
-			ModeD = FSMC_ACCESS_MODE_D,
-		};
+        /// <summary>
+        ///		* 单位：HCLK 的周期数
+        ///		* 取值范围：[0, 15]
+        /// </summary>
+        uint32_t BusTurnAroundDuration() const
+        {
+            return _handle.BusTurnAroundDuration;
+        }
 
-		AccessModeOption AccessMode() const
-		{
-			return static_cast<AccessModeOption>(_handle.AccessMode);
-		}
-		void SetAccessMode(AccessModeOption value)
-		{
-			_handle.AccessMode = static_cast<uint32_t>(value);
-		}
-	};
-}
+        void SetBusTurnAroundDuration(uint32_t value)
+        {
+            _handle.BusTurnAroundDuration = value;
+        }
+
+        /// <summary>
+        ///		* 单位：HCLK 的周期数
+        ///		* 取值范围：[2, 16]
+        /// </summary>
+        uint32_t ClockDivision() const
+        {
+            return _handle.CLKDivision;
+        }
+
+        void SetClockDivision(uint32_t value)
+        {
+            _handle.CLKDivision = value;
+        }
+
+        uint32_t DataLatency() const
+        {
+            return _handle.DataLatency;
+        }
+
+        void SetDataLatency(uint32_t value)
+        {
+            _handle.DataLatency = value;
+        }
+
+        enum class AccessModeOption
+        {
+            ModeA = FSMC_ACCESS_MODE_A,
+            ModeB = FSMC_ACCESS_MODE_B,
+            ModeC = FSMC_ACCESS_MODE_C,
+            ModeD = FSMC_ACCESS_MODE_D,
+        };
+
+        AccessModeOption AccessMode() const
+        {
+            return static_cast<AccessModeOption>(_handle.AccessMode);
+        }
+
+        void SetAccessMode(AccessModeOption value)
+        {
+            _handle.AccessMode = static_cast<uint32_t>(value);
+        }
+    };
+} // namespace hal
